750a74de1bbce04ee87872b4caee00551eea2b7d
Ryan Moodie <ryanmoodie@gmail.com>
2021-04-14 19:23:56 +0100

    migrate local hooks to program pre-commit

--- init.sh
+++ init.sh
@@ -5,22 +5,20 @@ echo "Initialising local repo git hooks..."
 echo "Setting git hooks path to .githooks"
 git config core.hooksPath .githooks
 
-echo "Checking that black is installed..."
-if ! type black; then
-    echo "Installing black..."
-    python3 -m pip install --user black
+echo "Checking that pre-commit is installed..."
+if ! type pre-commit; then
+    echo "Installing pre-commit..."
+    python3 -m pip install --user pre-commit
 fi
 
-echo "Checking that clang-format is installed..."
-if ! type clang-format; then
-    echo "Installing clang-format..."
-    python3 -m pip install --user clang-format
+if ! type pre-commit > /dev/null; then
+    echo "I tried to install pre-commit but I can't find it. It's up to you now to install it or add it to your path!"
 fi
 
-echo "Checking that flake8 is installed..."
-if ! type flake8; then
-    echo "Installing flake8..."
-    python3 -m pip install --user flake8
-fi
+# echo "Checking that clang-format is installed..."
+# if ! type clang-format; then
+#     echo "Installing clang-format..."
+#     python3 -m pip install --user clang-format
+# fi
 
 echo "Successfully completed initialisation!"
