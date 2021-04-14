9d8cac90b8b7457c967e5c608c92e55c4fab19ab
Ryan Moodie <ryanmoodie@gmail.com>
2021-04-14 16:39:07 +0100

    install linters in init

--- init.sh
+++ init.sh
@@ -1,1 +1,20 @@
+#!/usr/bin/sh
+
+echo "Initialising local repo git hooks..."
+
+echo "Setting git hooks path to .githooks"
 git config core.hooksPath .githooks
+
+echo "Checking that black is installed..."
+if ! type black; then
+    echo "Installing black..."
+    python3 -m pip install --user black
+fi
+
+echo "Checking that clang-format is installed..."
+if ! type clang-format; then
+    echo "Installing clang-format..."
+    python3 -m pip install --user clang-format
+fi
+
+echo "Successfully completed initialisation!"
