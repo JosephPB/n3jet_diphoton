ddc3f443ff91f3bc1f4b7c7ab221156e1b840fe6
Ryan Moodie <ryanmoodie@gmail.com>
2021-04-16 08:55:26 +0100

    testing floats with C++ NN

--- interface/test/test.cpp
+++ interface/test/test.cpp
@@ -11,9 +11,6 @@
 double d(double a, double b) { return 2 * std::abs((a - b) / (a + b)); }
 
 int main() {
-  std::cout.setf(std::ios_base::scientific, std::ios_base::floatfield);
-  std::cout.precision(16);
-
   std::cout << '\n'
             << "n3jet: test pretrained neural network C++ inference result against "
                "reference Python implementation, showing relative difference d"
@@ -24,9 +21,12 @@ int main() {
   const int pspoints{2};
   const int training_reruns{20};
   const double delta{0.02};
+  const std::string model_dir{
+      "../../models/3g2a/RAMBO/"
+      "events_100k_fks_all_legs_all_pairs_new_sherpa_cuts_pdf_njet_test/"};
+  const std::string cut_dir{"cut_0.02/"};
 
-  // raw momenta input
-  std::vector<std::vector<std::vector<double>>> momenta{{
+  const std::vector<std::vector<std::vector<double>>> momenta_f64{{
       {
           {80.80323390148537, 0.0, 0.0, 80.8032339014854},
           {53.57113343015938, 0.0, 0.0, -53.571133430159385},
@@ -49,27 +49,49 @@ int main() {
       },
   }};
 
+  std::vector<std::vector<std::vector<float>>> momenta_f32(
+      pspoints, std::vector<std::vector<float>>(legs, std::vector<float>(4)));
+  for (int k{0}; k < pspoints; ++k) {
+    for (int j{0}; j < legs; ++j) {
+      for (int i{0}; i < 4; ++i) {
+        momenta_f32[k][j][i] = static_cast<float>(momenta_f64[k][j][i]);
+      }
+    }
+  }
+
   std::array<double, 2> python_outputs{{2.2266408e-07, 1.430258598666967e-06}};
 
-  nn::FKSEnsemble<double> ensemble(
-      legs, training_reruns,
-      "../../models/3g2a/RAMBO/"
-      "events_100k_fks_all_legs_all_pairs_new_sherpa_cuts_pdf_njet_test/",
-      delta, "cut_0.02/");
+  nn::FKSEnsemble<float> ensemble_f32(legs, training_reruns, model_dir, delta, cut_dir);
+
+  nn::FKSEnsemble<double> ensemble_f64(legs, training_reruns, model_dir, delta,
+                                       cut_dir);
 
   for (int i{0}; i < pspoints; ++i) {
     std::cout << "==================== Test point " << i + 1
               << " ====================" << '\n';
 
-    double average_output{ensemble.compute(momenta[i])};
+    ensemble_f32.compute_with_error(momenta_f32[i]);
 
-    ensemble.compute_with_error(momenta[i]);
+    double average_output{ensemble_f64.compute(momenta_f64[i])};
 
-    std::cout << std::setw(17) << "Python = " << python_outputs[i] << '\n'
-              << std::setw(17) << "C++ = " << average_output << '\n'
-              << std::setw(17) << "C++ 2 = " << ensemble.mean << " +- "
-              << ensemble.std_err << '\n'
-              << std::setw(17)
-              << "d(C++, Python) = " << d(average_output, python_outputs[i]) << '\n';
+    ensemble_f64.compute_with_error(momenta_f64[i]);
+
+    const int cw{20};
+
+    std::cout << std::scientific << std::setprecision(16) << std::setw(cw)
+              << "Python = " << python_outputs[i] << '\n'
+              << std::setw(cw) << "C++ f64 = " << average_output << '\n'
+              << std::setw(cw) << "C++ f32 w/ err = " << ensemble_f32.mean << " ± "
+              << std::setprecision(1) << ensemble_f32.std_err << " (" << std::fixed
+              << 100 * ensemble_f32.std_err / ensemble_f32.mean << "%)" << '\n'
+              << std::setw(cw) << "C++ f64 w/ err = " << std::scientific
+              << std::setprecision(16) << ensemble_f64.mean << " ± "
+              << std::setprecision(1) << ensemble_f64.std_err << " (" << std::fixed
+              << 100 * ensemble_f64.std_err / ensemble_f64.mean << "%)" << '\n'
+              << std::setw(cw) << "d(C++ f32, Python) = " << std::scientific
+              << d(ensemble_f32.mean, python_outputs[i]) << '\n'
+              << std::setw(cw)
+              << "d(C++ f64, Python) = " << d(ensemble_f64.mean, python_outputs[i])
+              << '\n';
   }
 }
