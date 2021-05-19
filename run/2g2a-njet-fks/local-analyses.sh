6efa2a618d86597d200ca59e42376b221251afcd
ryan <ryanmoodie@gmail.com>
2021-05-19 14:02:59 +0100

    set up 2g2a run

--- run/2g2a-njet-fks/local-analyses.sh
+++ run/2g2a-njet-fks/local-analyses.sh
@@ -9,5 +9,5 @@ total_events=$3
 run_events=$(($total_events / $num_runs))
 
 for rseed in $(seq -w ${init_seed} ${end_seed}); do
-    RIVET_ANALYSIS_PATH=../../analysis/diphoton-1tev/ Sherpa -f Run.dat -R $rseed -e $run_events -A Analysis.$rseed >${rseed}.analysis.log 2>&1 &
+    RIVET_ANALYSIS_PATH=../../analysis/diphoton-1tev/ Sherpa -f Run.dat -R $rseed -e $run_events -A Analysis.$rseed >${rseed}.analysis.log 2>${rseed}.analysis.err &
 done
