79aa26a592c6a136330286476d1a292e19196cb8
Ryan Moodie <ryanmoodie@gmail.com>
2021-05-26 19:59:17 +0100

    example of run with errors directory structure

---
+++ run/3g2a-nn-fks-errors/run.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+rseed=1
+run_events=100
+
+for ((nnrun = 0; nnrun < 20; nnrun++)); do
+    dir=run-${nnrun}
+    mkdir -p ${dir}
+    mkdir -p ${dir}/interface
+    mkdir -p ${dir}/run
+
+    perl -p -e "s|LLLLL|21|g; s|GGGGG|Interface${nnrun}|g; s|JJJJJ|1|g;" template.Run.dat >${dir}/run/Run.${nnrun}.dat
+
+    perl -p -e "s|AAAAA|${nnrun}|g;" template.Makefile.run >${dir}/run/Makefile
+
+    perl -p -e "s|AAAAA|${nnrun}|g;" template.Makefile.interface >${dir}/interface/Makefile
+
+    cd ${dir}/interface
+
+    ln -s ../../../interface/generic/model_fns.cpp .
+    ln -s ../../../interface/generic/model_fns.h .
+    ln -s ../../../interface/generic/interface.cpp .
+    ln -s ../../../interface/generic/interface.hpp .
+
+    make -j
+
+    cd ../run
+
+    ln -s ../interface/libInterface${nnrun}.so .
+
+    name=${nnrun}.${rseed}.analysis
+    RIVET_ANALYSIS_PATH=../../../../analysis/diphoton-1tev Sherpa -f Run.${nnrun}.dat -R ${rseed} -e ${run_events} -A ${name} >${name}.out 2>${name}.err &
+
+    cd ../..
+done
