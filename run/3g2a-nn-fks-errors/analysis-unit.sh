da091bcd196dc3b0610fbb3b7bf7e1eeb6a154d6
Joseph Aylett-Bullock <JosephBullock95@hotmail.co.uk>
2021-05-27 12:05:01 +0100

    updating analysis scripts

--- run/3g2a-nn-fks-errors/analysis-unit.sh
+++ run/3g2a-nn-fks-errors/analysis-unit.sh
@@ -8,7 +8,9 @@ for ((nnrun = 0; nnrun < 20; nnrun++)); do
     cd ${dir}/run
     cp ${nnrun}.${rseed}.analysis.yoda Analysis-nn-${nnrun}.yoda
 
-    RIVET_ANALYSIS_PATH=../../../../analysis/diphoton-1tev/ rivet-mkhtml ../../Analysis-njet.yoda Analysis-nn-${nnrun}.yoda --errs -t "NJet vs NN for run ${rseed}"
+    RIVET_ANALYSIS_PATH=../../../../analysis/diphoton-1tev/ rivet-mkhtml ../../unit-3M/Analysis-unit.yoda Analysis-nn-${nnrun}.yoda --errs -t "NJet vs NN for run ${rseed}"
+
+    cp -r rivet-plots/ ../../unit-3M/rivet-plots-${nnrun}/
 
     cd ../..
 
